a8e9314789370a649a8e34f3eeba0e6f81107ea9
Arham Shams <muhammad.arham1019@gmail.com>
2023-07-24 12:49:01 +0500

    Added an api to test the chains

---
+++ src/Pages/api/chat.ts
@@ -0,0 +1,26 @@
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeClient } from "pinecone-client";
+import { mrchain } from "chains/multiretrievalchain.js";
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { pinecone } from "chains/pinecone-client.js";
+const index = pinecone.Index('ifrs-v1');
+const vectorStore = await PineconeStore.fromExistingIndex(
+  new OpenAIEmbeddings({}),
+   {
+     pineconeIndex: index,
+     textKey: 'text'
+   }
+);
+
+
+
+const chain=mrchain(vectorStore,(token:string)=>{
+  console.log("chain runs");
+});
+
+const response=chain.call({
+  input:"Hello"
+}
+  
+);
+console.log("response:",response)
